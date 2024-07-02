5cba7a2edc02b0dcd32594acf21a6c74ac34d934
Alex <alex.kleinman@gmail.com>
2024-07-02 12:14:20 -0400

    completed lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -8,22 +8,26 @@ class Author:
     def __init__(self,name):
         self.name = name
         self.all.append(self)
-    def contracts(self):
+    def contracts(self): 
         return [contract for contract in Contract.all if contract.author == self]
     def books(self):
-        return [book for book in Book.all if book.author == self]
-    def sign_contract(book,date,royalties):
-        return Contract.all.append(book,date,royalties)
-    def total_royalties():
-        return [royalty for royalty in Contract.all if royalty.author == Author]
+        return [contract.book for contract in Contract.all if contract.author == self]
+    def sign_contract(self,book,date,royalties):
+        return Contract(self, book, date, royalties)
+    def total_royalties(self):
+        return sum([contract.royalties for contract in Contract.all if contract.author == self])
+    
 
 class Book:
-
     all = []
 
     def __init__(self,title):
         self.title = title
         self.all.append(self)
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    def authors(self):
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 class Contract:
 
@@ -36,47 +40,62 @@ class Contract:
         self.royalties = royalties
         self.all.append(self)
 
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
+
+
     @property 
     def author(self):
-        self._author
+       return self._author
     
     @author.setter
     def author(self,author):
-        if isinstance(author,str):
+        if isinstance(author,Author):
             self._author = author
+            author.all.append(self)
         else:
-            raise Exception("Nope")
+            raise TypeError("Invalid author. Expected instance of Author class.")
 
     @property 
     def book(self):
-        self._book
+       return self._book
 
     @book.setter
     def book(self,book):
-        if isinstance(book,str):
+        if isinstance(book,Book):
             self._book = book
+            book.all.append(self)
         else: 
-            raise Exception("Nope")
+            raise TypeError("Invalid book. Expected instance of Book class.")
 
     @property
     def date(self):
-        self._date
+       return self._date
 
     @date.setter
     def date(self,date):
-        self._date = date
+        if isinstance(date,str):
+            self._date = date
+        else: 
+            raise TypeError("Invalid date. Expected data type 'string'.")
     
     @property
     def royalties(self):
-        self._royalties
+       return self._royalties
 
     @royalties.setter
     def royalties (self,royalties):
-        self._royalties = royalties
+        if isinstance (royalties,int):
+            self._royalties = royalties
+        else: 
+            raise TypeError("Invalid input. Royalties must be integers.")
 
-Guy = Author("Guy")
-BestEver = Book("BestEver")
-Contract1 = Contract(Guy,BestEver)
-print(Guy.name)
-print(BestEver.title)
+
+author = Author("Guy")
+book = Book("The Good Book")
+contract = Contract(author,book,"9/9/9",9000)
+contract = author.sign_contract(book, "9/9/9",9000)
+print(author.contracts())
+print(author.books())
 print(Contract.all)
